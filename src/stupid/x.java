8631da50bd746692bd4bb8dd66c0965ef4c91d84
Đào Anh Huy <43365727+HuyKnife@users.noreply.github.com>
2018-10-16 00:23:22 +0700

    Change code to match the convention

--- src/stupid/x.java
+++ src/stupid/x.java
@@ -1,3 +1,21 @@
+/**************************************************
+*	16050311
+*
+*	Version information stupid_code ver 2.0
+*
+*	Date 16-Oct-2018
+*
+*	Copyright notice
+*
+*	Modification Logs:
+*	DATE	16-Oct-2018
+*	
+*	AUTHOR	HuyKnife
+*	
+*	DESCRIPTION Change code to match the convention
+*	
+***************************************************/
+
 package stupid;
 
 import java.io.IOException;
@@ -5,13 +23,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class x {
-    public static void main(String[] arg) {
+public class x 
+{
+    public static void main(String[] arg) 
+    {
         int CC = hohoho(arg);
         System.exit(CC);
     }
 
-    public static int hohoho(String uuu[]) {
+    public static int hohoho(String uuu[]) 
+    {
         try {
             if (uuu.length != 2)
                 return -1;
@@ -22,18 +43,21 @@ public class x {
                 return -2;
             int RRR = 0;
             float eee = 0;
-            for (int aaa = 0; aaa < kkk - 3; aaa++) {
+            for (int aaa = 0; aaa < kkk - 3; aaa++) 
+            {
                 for (int qqq = 0; qqq < ddd.length() - 4; qqq += 1) {
                     int vOv = 0;
                     for (int CC = 0; CC < 4; CC++) {
                         if (bbb.charAt(aaa + CC) == ddd.charAt(qqq + CC))
                             vOv++;
                     }
-                    if (vOv == 4) {
+                    if (vOv == 4) 
+                    {
                         System.out.println("DEBUG:".concat(String.valueOf(aaa) + "," + String.valueOf(qqq)));
                         RRR++;
                         eee += 1;
-                    } else
+                    } 
+                    else
                         ++RRR;
                 }
             }
